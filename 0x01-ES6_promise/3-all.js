77a417fc7b16da91c75c8483be276d1f68898e16
georgeeset <georgeperfect4u@gmail.com>
2023-09-28 07:28:49 +0100

    attemptingint to return promise to main function

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
-import { uploadPhoto } from './utils';
-import { createUser } from './utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all(uploadPhoto(), createUser()).then((message) => {
-    const { body } = message[0];
-    const { firstName, lastName } = message[1];
-    console.log(`${body} ${firstName} ${lastName}`);
-  })
+  return Promise.all(uploadPhoto(), createUser())
+    .then((message) => {
+      const { body } = message[0];
+      const { firstName, lastName } = message[1];
+      console.log(`${body} ${firstName} ${lastName}`);
+    })
     .catch(() => console.log('Signup system offline'));
 }
