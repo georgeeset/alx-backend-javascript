c7e1b1b27c44eece2d94af5c50889d9cbe04340d
georgeeset <georgeperfect4u@gmail.com>
2023-09-28 07:08:28 +0100

    handle multiple successful promices

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto } from "./utils";
+import { createUser } from "./utils";
+
+export default function handleProfileSignup() {
+  Promise.all(uploadPhoto(), createUser()).then((message) => {
+    const { body } = message[0];
+    const { firstName, lastName } = message[1];
+    console.log(`${body} ${firstName} ${lastName}`);
+  })
+    .catch(() => console.log('Signup system offline'));
+}
