b9032843a511743291e72ee3d97f1b77fd4ed9eb
georgeeset <georgeperfect4u@gmail.com>
2023-09-28 08:15:15 +0100

    printing all responce in one place

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all(uploadPhoto(), createUser())
-    .then((message) => {
-      const { body } = message[0];
-      const { firstName, lastName } = message[1];
-      console.log(`${body} ${firstName} ${lastName}`);
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((responce) => {
+      console.log(`${responce[0].body} ${responce[1].firstName} ${responce[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
