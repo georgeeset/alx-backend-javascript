0ab0c2e65dd0b207daf39d0dc353f0b6a93289bf
georgeeset <georgeperfect4u@gmail.com>
2023-09-29 19:00:43 +0100

    building abscract class with checks in the constructor

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,20 @@
+class Building {
+  constructor(sqft) {
+    if (
+      typeof this.evacuationWarningMessage !== 'function' &&
+      this.constructor !== Building
+    ) {
+      throw Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
+
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
+
+export default Building;
