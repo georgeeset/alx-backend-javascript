ba8d5b56818209d4d2492632fe0ee419dec6a866
georgeeset <georgeperfect4u@gmail.com>
2023-10-04 21:23:40 +0100

    render table on browser with vanela and typescript

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,39 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+};
+
+var student1: Student = {
+    firstName: "George",
+    lastName: "Eset",
+    age: 32,
+    location: "Lagos"
+};
+
+var student2: Student = {
+    firstName: "Joy",
+    lastName: "Esetevbe",
+    age: 27,
+    location: "Ogun",
+};
+
+const studentsList: Student[] = [student1, student2];
+const table: HTMLTableElement = document.createElement('table');
+
+document.body.appendChild(table);
+const head: HTMLTableSectionElement = table.createTHead();
+const row: HTMLTableRowElement = head.insertRow();
+const th1: HTMLTableHeaderCellElement = row.insertCell(0);
+const th2: HTMLTableHeaderCellElement = row.insertCell(1);
+th1.innerHTML = ("<b>First Name</b>");
+th2.innerHTML = ("<b>Location</b>");
+const body: HTMLTableSectionElement = table.createTBody();
+studentsList.map((student) => {
+	const newRow: HTMLTableRowElement = body.insertRow();
+	const firstNameRow: HTMLTableCellElement = newRow.insertCell();
+	const locationRow: HTMLTableCellElement= newRow.insertCell();
+	firstNameRow.innerHTML = student.firstName;
+	locationRow.innerHTML = student.location;
+})
