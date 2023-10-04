71523ad37aa8818148ded034fdb3bfdf169f63cc
georgeeset <georgeperfect4u@gmail.com>
2023-10-04 09:48:36 +0100

    10-update lint worked on

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,7 @@
 export default function updateUniqueItems(groceries) {
-  if (Object.getPrototypeOf(groceries) !== Map.prototype)
+  if (Object.getPrototypeOf(groceries) !== Map.prototype) {
     throw TypeError('Cannot process');
+  }
 
   groceries.forEach((value, key) => {
     if (value === 1) {
